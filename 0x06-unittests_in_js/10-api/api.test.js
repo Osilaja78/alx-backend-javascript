0277a4cdebc4309e35007bd444cc35bdb6e69dcf
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2024-03-01 16:24:04 +0100

    code for task 10

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable jest/no-test-callback */
 /* eslint-disable jest/valid-expect */
 /* eslint-disable jest/prefer-expect-assertions */
@@ -25,6 +26,29 @@ describe('API integration test', () => {
   it('Ensure /cart/:id returns 404 when id is not a valid number', (done) => {
     request.get('http://localhost:7865/cart/abc', (_err, res) => {
       expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('Ensure /available_payments returns valid response', (done) => {
+    request.get('http://localhost:7865/available_payments', (_err, res, body) => {
+      expect(JSON.parse(body)).to.be.deep.equal(
+        {
+          payment_methods: {
+            credit_cards: true,
+            paypal: false
+          }
+        }
+      );
+      expect(res.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+
+  it('Ensure /login returns valid response', (done) => {
+    request.post('http://localhost:7865/login', { json: { userName: 'Alamedo' } }, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome Alamedo');
       done();
     });
   });
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -6,7 +6,7 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('API integration test', () => {
-  it('GET / returns correct response', (done) => {
+  it('Ensure / returns correct response', (done) => {
     request.get('http://localhost:7865', (_, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
