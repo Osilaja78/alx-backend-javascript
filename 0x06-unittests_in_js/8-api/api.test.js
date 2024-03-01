dd369e2dfaaddfab08e4efa0e1a3aa09e968ba11
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2024-03-01 15:40:59 +0100

    code for task 8

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,16 @@
+/* eslint-disable jest/no-test-callback */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/lowercase-name */
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API integration test', () => {
+  it('GET / returns correct response', (done) => {
+    request.get('http://localhost:7865', (res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
